6c5ae4e4c28bfc7e3f676ce6cc3313c7270db224
Le Quoc Viet <vietlq85@gmail.com>
2018-04-30 13:53:13 +0100

    initial commit

---
+++ 002-circle-square-triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from z3 import *
+
+circle, square, triangle = Ints('circle square triangle')
+s = Solver()
+
+s.add(circle + circle == 10)
+s.add(circle*square + square == 12)
+s.add(circle*square - triangle*circle == circle)
+
+print(s.check())
+print(s.model())
---
+++ README.md
@@ -0,0 +1,5 @@
+# Z3 SMT
+
+## References
+
+* [https://yurichev.com/writings/SAT\_SMT\_draft-EN.pdf](https://yurichev.com/writings/SAT_SMT_draft-EN.pdf)
